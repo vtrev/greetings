8b3c55ef55a4c1522dfdb423fa96067029ae8f9e
vtrev <baloyi.trev@gmail.com>
2018-05-05 13:04:33 +0200

    added GreetingsFactory function, added setting language tests and re factored the functions accordingly

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,9 +1,29 @@
 describe('greet function', function () {
-//var greet = 
+    //var greet = 
 
-    it('Dsiplay the user\'s name correctly', function () {
-        assert.equal('Hello Spock!', greet('Spock'));
+    it('Should dsiplay the user\'s name correctly', function () {
 
+        var greet = GreetingsFactory();
+
+
+        assert.equal('Hello Spock!', greet.greetNow('Spock','English'));
+        
     });
+    
+    
+    it('Should test if the setLang function sets the correct language ', function () {
+        var greet = GreetingsFactory();
+        
+        var name = 'Vusi'
+        var lang = greet.setLang('English');
+        var name1 = 'Spock'
+        var lang1 = greet.setLang('Zulu');
+        
+        assert.equal('Saubona Spock!',greet.greetNow(name1,lang1));
+        
+        assert.equal('Hello Vusi!', greet.greetNow(name,lang));
+
+
+    })
 
 });
