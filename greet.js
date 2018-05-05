2c63bb799cba8aca714c37514916b2641fec318f
vtrev <baloyi.trev@gmail.com>
2018-05-05 03:48:23 +0200

    created html,js and css files

---
+++ greet.js
@@ -0,0 +1,12 @@
+var displayName = document.querySelector('#displayName');
+
+var getInput = document.querySelector('#inputBox');
+
+var greetBtn = document.querySelector('#greetBtn');
+
+var greet = function(name){
+
+    return 'Hello ' + name +'!';
+}
+
+//greetBtn.addEventListener('click',greetFunctio
---
+++ test/greet.test.js
@@ -0,0 +1,9 @@
+describe('greet function', function () {
+//var greet = 
+
+    it('Dsiplay the user\'s name correctly', function () {
+        assert.equal('Hello Spock!', greet('Spock'));
+
+    });
+
+});
