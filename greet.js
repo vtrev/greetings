619af5e7760aeae2dfb22b6b6c9332b971f95984
vtrev <baloyi.trev@gmail.com>
2018-05-09 01:43:40 +0200

    added advanced functions,fixed mobile resonsiveness,styled radio buttons

--- greet.js
+++ greet.js
@@ -10,39 +10,58 @@ var greetBtn = document.querySelector('#greetBtn');
 var resetBtn = document.querySelector('#resetBtn');
 
 var displayCount = document.querySelector('#countNumber');
-//a global session counter to kee track of how many greets there are
-var counter = 0;
-
-//get the name of the user from the textbox
+//get the name of the user from the textbox 
 var getName = function () {
+    console.log('checking name...');
     var name = getInput.value;
     name = name.trim();
-    //Make the first letter of the name Uppercase
-    //name = name.charAt(0).toUpperCase();
+    //Make sure the first letter of the name is Uppercase
+    tmpString = name.substr(1, name.length);
+    firstCh = name.charAt(0).toUpperCase();
     return {
-        name
+        name: firstCh + tmpString
     }
 }
+
 //function to cear the textbox 
 
 var clearBox = function () {
     getInput.value = "";
 }
 
-//set the total counts of greetings
-var setCounter = function(){
+//display the total counts of greetings
+var setCounter = function () {
 
     displayCount.innerHTML = localStorage.getItem('counter');
-    
 }
-var checkCounter = function (){
-    
-    if(localStorage['counter'] === undefined){
-        let zero = JSON.stringify(0);
-        localStorage.setItem('counter',zero)
-    }counter++;
-    
+
+var checkCounter = function () {
+    //counter init 
+    if (localStorage['counter'] === undefined) {
+        //let zero = ;
+        localStorage.setItem('counter',JSON.stringify(0));
+    }
+
 }
+var checkMap = function () {
+    //console.log(localStorage.getItem('userMap'));
+    if (!localStorage.getItem('userMap')) {
+        var emptyObj = {}
+        //map =
+        localStorage.setItem('userMap', JSON.stringify(emptyObj));
+        console.log('map created');
+        console.log(JSON.parse(localStorage.getItem('userMap')));
+
+    } else {
+        //map =
+
+        console.log('map was there ');
+        console.log(JSON.parse(localStorage.getItem('userMap')));
+
+    }
+
+}
+
 
 
 
@@ -51,24 +70,35 @@ var checkCounter = function (){
 
 // the greetings factory functions 
 
-var GreetingsFactory = function () {
+var GreetingsFactory = function (userMap) {
 
-
-//function to takes in the user's name + their language as parameters and return a greeting accordingly
+    var greetMap = userMap;
     var greetNow = function (inputName, language) {
 
+        if (greetMap[inputName] === undefined) {
+            greetMap[inputName] = 0;
+            var counter = JSON.parse(localStorage.getItem('counter'));
+
+            localStorage.setItem('counter', JSON.parse(counter + 1));
+
+        }
         if (language === 'English') {
             return 'Hello ' + inputName + '!'
         }
         if (language === 'Zulu') {
 
             return 'Saubona ' + inputName + '!'
-            
+
+
+        }
+
+        if (language === 'Tsonga') {
+
+            return 'Avuxeni ' + inputName + '!'
+
 
         }
     }
-
-//function to take in(from the DOM) and return the user's desired language and return it
     var setLang = function (value) {
         var lang = '';
         if (value === 'English') {
@@ -77,23 +107,24 @@ var GreetingsFactory = function () {
         if (value === 'Zulu') {
             lang = 'Zulu';
         }
+        if (value === 'Tsonga') {
+            lang = 'Tsonga';
+        }
         return lang
     }
 
-
+    //functieeton to take in(from the DOM) and return the user's desired language and return it
 
 
     return {
         greetNow,
-        setLang
+        setLang,
+        greetMap
     }
+
 
 }
 
-
-//an instance of the Greetings factory function
-
-var greet = GreetingsFactory();
 
 
 
@@ -102,26 +133,47 @@ var greet = GreetingsFactory();
 
 greetBtn.addEventListener('click', function run() {
 
-    var radioValue = document.querySelector('input[name="radioLang"]:checked').value;
-    getName();
-    var langFromDom = greet.setLang(radioValue)
-    var nameFromDom = getName().name;
-    displayName.innerHTML = greet.greetNow(nameFromDom, langFromDom);
-    clearBox();
-    
-    //counter shit
+    checkMap();
 
-    checkCounter();
-    
-    var storageCount = JSON.parse(localStorage.getItem('counter')); //parseFloat(localStorage.getItem('counter'));
-   
-    localStorage.setItem('counter', storageCount += 1);
-    
-    setCounter();
+    var greet = GreetingsFactory(JSON.parse(localStorage.getItem('userMap')));
+
+    var radioBtn = document.querySelector('input[name="radioLang"]:checked');
+    var nameFromDom = getName().name;
+    if (nameFromDom !== "") {
+        if (radioBtn !== null) {
+
+            getName();
+            //console.log(greet.setLang('Zulu'));
+            var langFromDom = greet.setLang(radioBtn.value);
+
+            console.log(JSON.parse(localStorage.getItem('userMap')));
+
+            //counter 
+            checkCounter();
+
+            displayName.innerHTML = greet.greetNow(nameFromDom, langFromDom);
+            localStorage.setItem('userMap', JSON.stringify(greet.greetMap));
+
+            setCounter();
+            clearBox();
+        } else {
+            alert('Please choose your language first');
+        }
+
+
+
+    } else {
+
+        alert('Please type in your name first');
+    }
 
 });
-resetBtn.addEventListener('click',function run(){
-   localStorage.setItem('counter',JSON.stringify(0));
+
+
+resetBtn.addEventListener('click', function run() {
+    localStorage.setItem('counter', JSON.stringify(0));
+    localStorage.setItem('userMap', JSON.stringify({}));
+
     displayCount.innerHTML = 0;
-    
+
 });
